296278689d1cc72f100ea37681f62c5a1982ee8c
b1galez <b1galez@ad655f62-16bb-9e3d-ed23-7bd8a6b89f70>
2010-11-10 00:40:56 +0000

    Fixed issue 7: When more episodes are being downloaded, mysql sucks up a lot of cpu time.

--- downloadstatus.php
+++ downloadstatus.php
@@ -3,8 +3,20 @@
 	$showid = addslashes($_GET['showid']);
 	$season = intval(addslashes($_GET['season']));
 	$episode = intval(addslashes($_GET['id']));
-	$show = new Show($showid, NO_EPISODE_CHECK);
-	if (count($show->getEpisode($season.'x'.$episode)->getTorrents())>0) {
-		echo json_encode($show->getEpisode($season.'x'.$episode)->getDownloadStatus());
-	}	
+	$getHash = ($_GET['getHash'] == '1');
+	$hash = addslashes($_GET['hash']);
+	
+	if ($getHash) {	
+		$show = new Show($showid, NO_EPISODE_CHECK);
+		if (count($show->getEpisode($season.'x'.$episode)->getTorrents())>0) {
+			//echo json_encode($show->getEpisode($season.'x'.$episode)->getDownloadStatus());
+			$torrents = $show->getEpisode($season.'x'.$episode)->getTorrents();
+			echo json_encode($torrents[0]);
+			die();
+		}
+	} else {
+		$transmission = new Transmission();
+		$actualstatus = $transmission->getStatus($hash);
+		echo json_encode($actualstatus);
+	}
 ?>
